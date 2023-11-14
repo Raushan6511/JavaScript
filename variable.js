0c7fac8e48555a92605f51304a795704cd497af0
Raushan Kumar <raushankumar6511@gmail.com>
2023-11-14 08:36:27 +0530

    variable file

---
+++ variable.js
@@ -0,0 +1,11 @@
+var userName="Raushan Kumar" // but this is not recommended because it breaks the rule of 
+                             // scope( block & functional)
+let userId="raushan65"
+const userPassword=56456
+userEmail="raushankumar6511@gmail.com" // but this is not recommended
+
+let userAddress; // if we not initialize the let variable the by default it give undefined 
+
+// userPassword=8767867  we can not re-assign the constant variable
+
+console.table([userName,userId,userPassword,userEmail,userAddress])
